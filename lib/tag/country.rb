491305d7bac10220c15b5278c8456b1830f242a8
liuxianglin <lewislinlin@gmail.com>
2018-10-15 16:34:51 +0800

    update tag_country.yaml

--- lib/tag/country.rb
+++ lib/tag/country.rb
@@ -9,6 +9,13 @@ module Tag
       def list
         YAML.load_file("#{GEM_ROOT}/db/tag_country.yaml")["countries"].keys
       end
+
+
+      # 单个国家标签
+      def country_tag(country_name)
+        YAML.load_file("#{GEM_ROOT}/db/tag_country.yaml")["countries"][country_name]
+      end
+
     end
   end
 end
